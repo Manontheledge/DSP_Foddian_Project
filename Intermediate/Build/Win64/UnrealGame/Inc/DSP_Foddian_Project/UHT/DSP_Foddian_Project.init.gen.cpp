3b01c26fa6ce9170c37d225b1cb0cbb809df014b
Manontheledge <paul.ramos.0131@gmail.com>
2025-05-13 14:55:24 -0700

    more stuff

---
+++ Intermediate/Build/Win64/UnrealGame/Inc/DSP_Foddian_Project/UHT/DSP_Foddian_Project.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeDSP_Foddian_Project_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_DSP_Foddian_Project;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_DSP_Foddian_Project()
+	{
+		if (!Z_Registration_Info_UPackage__Script_DSP_Foddian_Project.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/DSP_Foddian_Project",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x4BF20326,
+				0x4A73564D,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_DSP_Foddian_Project.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_DSP_Foddian_Project.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_DSP_Foddian_Project(Z_Construct_UPackage__Script_DSP_Foddian_Project, TEXT("/Script/DSP_Foddian_Project"), Z_Registration_Info_UPackage__Script_DSP_Foddian_Project, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x4BF20326, 0x4A73564D));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
